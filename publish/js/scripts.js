f25c9281bd01d2fce208eab3c6dcd56b75a31733
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-02-06 17:47:48 -0600

    Adding a carousel to the home page.

--- publish/js/scripts.js
+++ publish/js/scripts.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
-	$('.sm').smartmenus(
-		{
-			showFunction: function($ul, complete) {
-				$ul.slideDown(250, complete);
-			},
-			hideFunction: function($ul, complete) {
-				$ul.slideUp(250, complete);
-			}
+	$('.sm').smartmenus({
+		showFunction: function($ul, complete) {
+			$ul.slideDown(250, complete);
+		},
+		hideFunction: function($ul, complete) {
+			$ul.slideUp(250, complete);
 		}
-	);
+	});
 
-	$("#nanoGallery3").nanoGallery(
-		{
-			thumbnailHeight: 'auto',
-			thumbnailHoverEffect: 'scaleLabelOverImage',
-			colorScheme: 'light'
-		}
-	);
+	$('.fade').slick({
+		dots: true,
+		infinite: true,
+		speed: 700,
+		fade: true,
+		cssEase: 'linear',
+		autoplay: true
+	});
+
+	$("#nanoGallery3").nanoGallery({
+		thumbnailHeight: 'auto',
+		thumbnailHoverEffect: 'scaleLabelOverImage',
+		colorScheme: 'light'
+	});
 });
