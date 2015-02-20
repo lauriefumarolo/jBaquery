77936928cffab2c286427368cf9d2b6bf7afb9c4
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-02-20 17:37:02 -0600

    Updating main navigation menu to look like jQuery website.

--- publish/js/scripts.js
+++ publish/js/scripts.js
@@ -1,10 +1,3 @@
 $(document).ready(function() {
-	$('.sm').smartmenus({
-		showFunction: function($ul, complete) {
-			$ul.slideDown(250, complete);
-		},
-		hideFunction: function($ul, complete) {
-			$ul.slideUp(250, complete);
-		}
-	});
+	
 });
