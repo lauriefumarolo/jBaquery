55e7357680f7ad447d52664b2f2e7689cfcb4bcf
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-02-25 12:05:15 -0600

    Cleanup and comments for jQuery files. Remove unnecessary nano gallery file. Move nano gallery jQuery code to common file.

---
+++ publish/js/nano_gallery_scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	// setup for nano gallery plugin
+	$("#nanoGallery").nanoGallery({
+		thumbnailHeight: 'auto',
+		thumbnailHoverEffect: 'scaleLabelOverImage',
+		colorScheme: 'light'
+	}); // end setup for nano gallery plugin
+}); // end document.ready()
--- publish/js/scripts.js
+++ publish/js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+	// setup for sidr (responsive site navigation drawer) plugin
 	$('#responsive-menu-button').sidr({
 		name: 'sidr',
-		side: 'right',
-		source: '#mainNav'
-	});
-});
+		side: 'right', // open drawer on right side of screen
+		source: '#mainNav' // use same navigation links as non-responsive navigation
+	}); // end setup for sidr plugin
+}); // end document.ready()
